2ccef29aa579591adb2069ca1e6b4fe0653823ab
Jarshan <jarshanjauffar@gmail.com>
2025-03-04 23:28:54 +0530

    Optimized queries with indexing

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,3 +1,4 @@
+
 const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
@@ -6,15 +7,13 @@ const itemSchema = new mongoose.Schema({
   price: { type: Number, required: true },
 });
 
-
-itemSchema.index({ name: 1 });
+itemSchema.index({ name: 1 }); 
 
 module.exports = mongoose.model("Item", itemSchema);
 
 
 
 
-const Item = require("./models/Item");  // Make sure this path is correct
-Item.collection.createIndex({ name: 1 })
+
   
   
