f091a2a61d3d6f9acbdae358fa0acf3d3b385eec
Jarshan <jarshanjauffar@gmail.com>
2025-03-04 19:04:26 +0530

    Optimized MongoDB Queries

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -6,4 +6,15 @@ const itemSchema = new mongoose.Schema({
   price: { type: Number, required: true },
 });
 
+
+itemSchema.index({ name: 1 });
+
 module.exports = mongoose.model("Item", itemSchema);
+
+
+
+
+const Item = require("./models/Item");  // Make sure this path is correct
+Item.collection.createIndex({ name: 1 })
+  
+  
