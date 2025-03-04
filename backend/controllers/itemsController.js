1186ea7afcc06ccbc809177b9944d09ae97937de
Jarshan <jarshanjauffar@gmail.com>
2025-03-04 23:30:19 +0530

    Pagination implement

--- backend/controllers/itemsController.js
+++ backend/controllers/itemsController.js
@@ -1,8 +1,6 @@
+
 const Item = require("../models/Item");
 
-// @desc    Get all items (with Pagination)
-// @route   GET /api/items
-// @access  Public
 const getItems = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
@@ -10,8 +8,17 @@ const getItems = async (req, res) => {
     const skip = (page - 1) * limit;
 
     const items = await Item.find().skip(skip).limit(limit);
+    const total = await Item.countDocuments(); // Total number of items
 
-    res.status(200).json({ success: true, data: items });
+    res.status(200).json({
+      success: true,
+      data: items,
+      pagination: {
+        page,
+        limit,
+        total,
+      },
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
