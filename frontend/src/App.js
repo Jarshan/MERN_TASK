84789fd91bc0ccf1efa8e20e05d41cfcc085f501
Jarshan <jarshanjauffar@gmail.com>
2025-03-05 10:29:44 +0530

    App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 
+import React from 'react';
 import './App.css';
+import ItemList from './components/ItemList';
 
 function App() {
-  
+  return (
+    <div className="App">
+      <h1>MERN Pagination with Infinite Scroll</h1>
+      <ItemList />
+    </div>
+  );
 }
 
 export default App;
